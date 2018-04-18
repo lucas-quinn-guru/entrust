4eb30113142633d309d36f4830d052a49defddfc
Marcellus Barrus <mbarrus73@gmail.com>
2018-04-17 20:02:26 -0600

    updating migration

--- src/views/generators/migration.blade.php
+++ src/views/generators/migration.blade.php
@@ -21,41 +21,35 @@ class EntrustSetupTables extends Migration
             $table->string('display_name')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
+            $table->softdeletes();
         });
 
         // Create table for associating roles to users (Many-to-Many)
-        Schema::create('{{ $roleUserTable }}', function (Blueprint $table) {
-            $table->integer('user_id')->unsigned();
-            $table->integer('role_id')->unsigned();
-
-            $table->foreign('user_id')->references('{{ $userKeyName }}')->on('{{ $usersTable }}')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('{{ $rolesTable }}')
-                ->onUpdate('cascade')->onDelete('cascade');
-
-            $table->primary(['user_id', 'role_id']);
-        });
-
-        // Create table for storing permissions
-        Schema::create('{{ $permissionsTable }}', function (Blueprint $table) {
+        Schema::create('roleables', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->unique();
             $table->string('display_name')->nullable();
             $table->string('description')->nullable();
             $table->timestamps();
+            $table->softdeletes();
+        });
+
+        // Create table for storing permissions
+        Schema::create('permissions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('display_name')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+            $table->softdeletes();
         });
 
         // Create table for associating permissions to roles (Many-to-Many)
-        Schema::create('{{ $permissionRoleTable }}', function (Blueprint $table) {
+        Schema::create('permissionables', function (Blueprint $table) {
             $table->integer('permission_id')->unsigned();
-            $table->integer('role_id')->unsigned();
-
-            $table->foreign('permission_id')->references('id')->on('{{ $permissionsTable }}')
-                ->onUpdate('cascade')->onDelete('cascade');
-            $table->foreign('role_id')->references('id')->on('{{ $rolesTable }}')
-                ->onUpdate('cascade')->onDelete('cascade');
-
-            $table->primary(['permission_id', 'role_id']);
+            $table->morphs("permissionable");
+            $table->timestamps();
+            $table->softdeletes();
         });
 
         DB::commit();
